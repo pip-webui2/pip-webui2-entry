8c1374f3a78be81345fbc5d3bccfa32db4a359a7
KateNegrienko <katya@negrienko.com>
2017-12-11 18:56:51 +0200

    [Signup] fix dummy component

--- src/signup/signup.component.ts
+++ src/signup/signup.component.ts
@@ -8,26 +8,51 @@ import {  Component, Input, Output, EventEmitter, ChangeDetectionStrategy  } fro
 })
 export class PipSignupComponent {
 
-	@Input() serverUrl: string;
-    @Input() email: string;
-    @Input() password: string;
-    @Input() error: string;
-    @Input() loading: boolean;
 
-    @Output() submit = new EventEmitter();
-    @Output() abort = new EventEmitter();
+    @Input() title: string = 'Sign up';
+    
+        @Input() showUrl: boolean = true;
+        @Input() serverUrlName: string = 'Server URL';
+        @Input() serverUrl: string;
+        @Input() serverUrlPattern: string = '^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})$';
+        @Input() serverUrlRequired: string = 'Server URL is required';
+        @Input() serverUrlInvalid: string = 'Server URL is invalid';
+    
+        @Input() emailName: string = 'Email';
+        @Input() email: string;
+        @Input() emailPattern: string = '^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$';
+        @Input() emailRequired: string = 'Email is required';
+        @Input() emailInvalid: string = 'Email URL is invalid';
 
-    public constructor() { }
-
-    public onSubmit(): void {
-        this.submit.emit({
-            serverUrl: this.serverUrl,
-            email: this.email,
-            password: this.password
-        });
-    }
-
-    public onAbort(): void {
-        this.abort.emit();
-    }
+        @Input() showName: boolean = true;
+        @Input() nameName: string = 'Full name';
+        @Input() name: string;
+        @Input() nameRequired: string = 'Name is required';
+    
+        @Input() passwordName: string = 'Password';
+        @Input() password: string;
+        @Input() passwordRequired: string = 'Password is required';
+        @Input() passwordInvalid: string = 'Must be a list 6 symbols';
+    
+        @Input() error: string;
+        @Input() loading: boolean;
+        @Input() signupName: string = 'Signup';
+        @Input() abortName: string = 'Abort';
+    
+        @Output() submit = new EventEmitter();
+        @Output() abort = new EventEmitter();
+    
+        public constructor() { }
+    
+        public onSubmit(): void {
+            this.submit.emit({
+                serverUrl: this.serverUrl,
+                email: this.email,
+                password: this.password
+            });
+        }
+    
+        public onAbort(): void {
+            this.abort.emit();
+        }
 }
