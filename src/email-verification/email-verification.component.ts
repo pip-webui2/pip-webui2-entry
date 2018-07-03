d7349da80e48306dc741e972425709a5ea1aa453
Anastas Fonotov <anastasf@gmail.com>
2018-07-03 19:01:55 +0300

    fix set default value

--- src/email-verification/email-verification.component.ts
+++ src/email-verification/email-verification.component.ts
@@ -8,44 +8,63 @@ import { REGEX_STRING_CODE, REGEX_STRING_EMAIL } from '../core/defaults';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PipEmailVerificationDummyComponent {
-    @Input() title: string = 'Verify email?';
+    @Input() title: string;
     @Input() subtitle: string;
 
-    @Input() changeServerUrlName: string = 'Change server';
-    @Input() codeName: string = 'Verification code';
+    @Input() changeServerUrlName: string;
+    @Input() codeName: string;
     @Input() code: string;
-    @Input() codePattern: string = REGEX_STRING_CODE;
-    @Input() codeRequired: string = 'Code is required';
-    @Input() codeInvalid: string = 'Code is invalid';
+    @Input() codePattern: string;
+    @Input() codeRequired: string;
+    @Input() codeInvalid: string;
 
-    @Input() success: string = 'Code send successfuly';
-    @Input() showSuccess: boolean = false;
+    @Input() success: string;
+    @Input() showSuccess: boolean;
 
-    @Input() emailName: string = 'Email';
+    @Input() emailName: string;
     @Input() email: string;
-    @Input() emailPattern: string = REGEX_STRING_EMAIL;
-    @Input() emailRequired: string = 'Email is required';
-    @Input() emailInvalid: string = 'Email URL is invalid';
+    @Input() emailPattern: string;
+    @Input() emailRequired: string;
+    @Input() emailInvalid: string;
 
     @Input() error: any;
     @Input() loading: boolean;
-    @Input() cancelName: string = 'Cancel';
-    @Input() verificationName: string = 'Verification';
-    @Input() abortName: string = 'Abort';
+    @Input() cancelName: string;
+    @Input() verificationName: string;
+    @Input() abortName: string;
 
-    @Input() resendText: string = "If you haven't received the code, press ";
-    @Input() resendText1: string = 'resend';
-    @Input() resendLinkText: string = ' to send it again.';
-    
+    @Input() resendText: string;
+    @Input() resendText1: string;
+    @Input() resendLinkText: string;
+
     @Input() showCancel: boolean = true;
-    
+
     @Output() submit = new EventEmitter();
     @Output() abort = new EventEmitter();
     @Output() cancel = new EventEmitter();
     @Output() resend = new EventEmitter();
 
-    public constructor() { 
+    public constructor() {
 
+    }
+
+    public ngOnInit() {
+        this.title = this.title === undefined ? 'Verify email?' : null;
+        this.changeServerUrlName = this.changeServerUrlName === undefined ? 'Change server' : null;
+        this.codeName = this.codeName === undefined ? 'Verification code' : null;
+        this.codePattern = this.codePattern === undefined ? REGEX_STRING_CODE : null;
+        this.codeRequired = this.codeRequired === undefined ? 'Code is required' : null;
+        this.codeInvalid = this.codeInvalid === undefined ? 'Code is invalid' : null;
+        this.emailName = this.emailName === undefined ? 'Email' : null;
+        this.emailPattern = this.emailPattern === undefined ? REGEX_STRING_EMAIL : null;
+        this.emailRequired = this.emailRequired === undefined ? 'Email is required' : null;
+        this.emailInvalid = this.emailInvalid === undefined ? 'Email URL is invalid' : null;
+        this.cancelName = this.cancelName === undefined ? 'Cancel' : null;
+        this.verificationName = this.verificationName === undefined ? 'Verification' : null;
+        this.abortName = this.abortName === undefined ? 'Abort' : null;
+        this.resendText = this.resendText === undefined ? "If you haven't received the code, press " : null;
+        this.resendText1 = this.resendText1 === undefined ? 'resend' : null;
+        this.resendLinkText = this.resendLinkText === undefined ? ' to send it again.' : null;
     }
 
     public onSubmit(): void {
--- src/phone-verification/phone-verification.component.ts
+++ src/phone-verification/phone-verification.component.ts
@@ -8,44 +8,63 @@ import { REGEX_STRING_CODE, REGEX_STRING_PHONE } from '../core/defaults';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PipPhoneVerificationDummyComponent {
-    @Input() title: string = 'Verify phone?';
+    @Input() title: string;
     @Input() subtitle: string;
 
-    @Input() changeServerUrlName: string = 'Change server';
-    @Input() codeName: string = 'Verification code';
+    @Input() changeServerUrlName: string;
+    @Input() codeName: string;
     @Input() code: string;
-    @Input() codePattern: string = REGEX_STRING_CODE;
-    @Input() codeRequired: string = 'Code is required';
-    @Input() codeInvalid: string = 'Code is invalid';
+    @Input() codePattern: string;
+    @Input() codeRequired: string;
+    @Input() codeInvalid: string;
 
-    @Input() success: string = 'Code send successfuly';
-    @Input() showSuccess: boolean = false;
-    
-    @Input() phoneName: string = 'Phone';
+    @Input() success: string;
+    @Input() showSuccess: boolean;
+
+    @Input() phoneName: string;
     @Input() phone: string;
-    @Input() phonePattern: string = REGEX_STRING_PHONE;
-    @Input() phoneRequired: string = 'Phone is required';
-    @Input() phoneInvalid: string = 'Enter a valid phone in e.164 format: +xxxxxxxxxxx';
+    @Input() phonePattern: string;
+    @Input() phoneRequired: string;
+    @Input() phoneInvalid: string;
 
     @Input() error: any;
     @Input() loading: boolean;
-    @Input() cancelName: string = 'Cancel';
-    @Input() verificationName: string = 'Verification';
-    @Input() abortName: string = 'Abort';
+    @Input() cancelName: string;
+    @Input() verificationName: string;
+    @Input() abortName: string;
 
-    @Input() resendText: string = "If you haven't received the code, press ";
-    @Input() resendText1: string = 'resend';
-    @Input() resendLinkText: string = ' to send it again.';
-    
+    @Input() resendText: string;
+    @Input() resendText1: string;
+    @Input() resendLinkText: string;
+
     @Input() showCancel: boolean = true;
-    
+
     @Output() submit = new EventEmitter();
     @Output() abort = new EventEmitter();
     @Output() cancel = new EventEmitter();
     @Output() resend = new EventEmitter();
 
-    public constructor() { 
+    public constructor() {
 
+    }
+
+    public ngOnInit() {
+        this.title = this.title === undefined ? 'Verify phone?' : null;
+        this.changeServerUrlName = this.changeServerUrlName === undefined ? 'Change server' : null;
+        this.codeName = this.codeName === undefined ? 'Verification code' : null;
+        this.codePattern = this.codePattern === undefined ? REGEX_STRING_CODE : null;
+        this.codeRequired = this.codeRequired === undefined ? 'Code is required' : null;
+        this.codeInvalid = this.codeInvalid === undefined ? 'Code is invalid' : null;
+        this.phoneName = this.phoneName === undefined ? 'Phone' : null;
+        this.phonePattern = this.phonePattern === undefined ? REGEX_STRING_PHONE : null;
+        this.phoneRequired = this.phoneRequired === undefined ? 'Phone is required' : null;
+        this.phoneInvalid = this.phoneInvalid === undefined ? 'Enter a valid phone in e.164 format: +xxxxxxxxxxx' : null;
+        this.cancelName = this.cancelName === undefined ? 'Cancel' : null;
+        this.verificationName = this.verificationName === undefined ? 'Verification' : null;
+        this.abortName = this.abortName === undefined ? 'Abort' : null;
+        this.resendText = this.resendText === undefined ? "If you haven't received the code, press " : null;
+        this.resendText1 = this.resendText1 === undefined ? 'resend' : null;
+        this.resendLinkText = this.resendLinkText === undefined ? ' to send it again.' : null;
     }
 
     public onSubmit(): void {
